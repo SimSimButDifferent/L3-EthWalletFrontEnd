376bfd726ad0ae23e873b643d4e6b4899993c842
simsim <simeon.campbell@live.com>
2024-01-07 13:05:27 +0700

    dev chains added to walletconnect

--- context/Web3Modal.tsx
+++ context/Web3Modal.tsx
@@ -3,7 +3,7 @@
 import { createWeb3Modal, defaultConfig } from "@web3modal/ethers/react"
 
 // 1. Get projectId at https://cloud.walletconnect.com
-const projectId = "YOUR_PROJECT_ID"
+const projectId = "cf80e8dfdf111fd756361f3117399405"
 
 // 2. Set chains
 const mainnet = {
@@ -14,17 +14,34 @@ const mainnet = {
     rpcUrl: "https://cloudflare-eth.com",
 }
 
+const sepolia = {
+    chainId: 11155111,
+    name: "Sepolia",
+    currency: "ETH",
+    explorerUrl: "https://sepolia.etherscan.io",
+    rpcUrl: "https://rpc.sepolia.org",
+
+}
+
+const localhost = {
+    chainId: 31337,
+    name: "Localhost 8545",
+    currency: "ETH",
+    explorerUrl: "",
+    rpcUrl: "http://localhost:8545",
+}
+
 // 3. Create modal
 const metadata = {
-    name: "My Website",
-    description: "My Website description",
+    name: "Eth Wallet",
+    description: "Simple Eth Wallet app created by Simeon Campbell",
     url: "https://mywebsite.com",
     icons: ["https://avatars.mywebsite.com/"],
 }
 
 createWeb3Modal({
     ethersConfig: defaultConfig({ metadata }),
-    chains: [mainnet],
+    chains: [mainnet, sepolia, localhost],
     projectId,
 })
 
