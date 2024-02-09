4f1e6b5937edeab39bcf1916d6a53b496106ad42
simsim <simeon.campbell@live.com>
2024-02-09 09:19:49 +0700

    user balance box added to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,12 +5,19 @@ import React from "react"
 const Header: React.FC = () => {
     return (
         <nav>
-            <div className="flex flex-cols-2 justify-end">
-                <div className="p-2">
-                    <w3m-button size="md" />
-                </div>
-                <div className="p-2">
-                    <w3m-network-button />
+            <div>
+                <div className="flex flex-cols-3 justify-end">
+                    <div className="p-2">
+                        <div className="flex px-2 py-1 font-semibold text-lg align-center border-opacity-15 border border-gray-300 rounded-full opacity-80">
+                            User Contract Balance: 0 ETH
+                        </div>
+                    </div>
+                    <div className="p-2">
+                        <w3m-button size="md" />
+                    </div>
+                    <div className="p-2">
+                        <w3m-network-button />
+                    </div>
                 </div>
             </div>
         </nav>
